d824ded02c04493371b7c953290d8fae9a02c5ca
CloneTrooper66 <luka.eli88@gmail.com>
2025-02-02 16:52:23 +0000

    users can now sign up for events

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/Dashboard";
 
+// app.jsx
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    const parsedUser = storedUser ? JSON.parse(storedUser) : null;
+
+    if (parsedUser) {
+      localStorage.setItem("role", parsedUser.role);
+    }
+    return parsedUser;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("role", user.role);
+    } else {
+      localStorage.removeItem("user");
+      localStorage.removeItem("role");
+    }
+  }, [user]);
 
   return (
     <Router>
@@ -14,6 +34,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register setUser={setUser} />} />
           <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/dashboard" element={<Dashboard user={user} />} />
         </Routes>
       </div>
     </Router>
